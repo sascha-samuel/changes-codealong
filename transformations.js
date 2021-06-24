f4a2888e2a77fc19c746658509746bbbfa393825
sascha-samuel <s.muff@steiner-hitech.at>
2021-06-24 19:55:22 +0200

    Transform more data

--- transformations.js
+++ transformations.js
@@ -31,15 +31,63 @@ export function aggregateAllChannels(
   inputMailchimp,
   inputStripe,
 ) {
-  return [];
+  return inputHubspot.map((hubspotUser) => {
+    // Identify by first name and last name because
+    // sometimes the email address doesn't match
+    const firstName = hubspotUser['First Name'].split(' ')[0];
+    const lastName = hubspotUser['Last Name'];
+    const mailchimpUser = inputMailchimp.find(
+      (user) => user.firstName === firstName && user.lastName === lastName,
+    );
+    const stripeTransaction = inputStripe.find((transaction) => {
+      const fName = transaction.name.split(' ')[0];
+      const lName = transaction.name.split(' ').pop();
+      return fName === firstName && lName === lastName;
+    });
+
+    return {
+      hubspotContactId: hubspotUser['Contact Id'],
+      firstName,
+      lastName,
+      hubspotLeadStatus: hubspotUser['Lead Status'],
+      hubspotRegisteredAt: hubspotUser['Registered At'],
+      hubspotTotalValue: hubspotUser['Total Value'],
+      mailchimpEmail: mailchimpUser.emailAddress,
+      mailchimpStatus: mailchimpUser.status,
+      mailchimpAudienceName: mailchimpUser.audienceName,
+      mailchimpSubscribeDate: mailchimpUser.subscribeDate,
+      stripeId: stripeTransaction?.id,
+      stripeEmail: stripeTransaction?.email,
+      stripeCreatedAt: stripeTransaction?.created_at_date,
+    };
+  });
 }
 
 export function deduplicate(input) {
-  return input;
+  return input.reduce((users, user) => {
+    const existingUser = users.find((u) => {
+      return (
+        u.id === user.id &&
+        u.name === user.name &&
+        u.email === user.email &&
+        u.created_at_date === user.created_at_date
+      );
+    });
+    if (!existingUser) users.push(user);
+    return users;
+  }, []);
 }
 
 export function cleanse(input) {
-  return input;
+  return input.map((user) => {
+    user['Lead Status'] = user['Lead Status']
+      .replaceAll('Interrrrrested', 'Interested')
+      .replaceAll('Customerr', 'Customer');
+
+    user['Registered At'] = user['Registered At'].replace(/s.+$/, '');
+
+    return user;
+  });
 }
 
 // Codealong: Data Analysis in Node.js
