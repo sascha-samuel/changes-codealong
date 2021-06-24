ec55231e36531188808f052ee72284bb418fb54c
sascha-samuel <s.muff@steiner-hitech.at>
2021-06-24 20:09:17 +0200

    Even more data transformed

--- transformations.js
+++ transformations.js
@@ -92,13 +92,57 @@ export function cleanse(input) {
 
 // Codealong: Data Analysis in Node.js
 export function getInterestedRepeatCustomers(inputHubspot, inputStripe) {
-  return [];
+  inputHubspot = cleanse(inputHubspot);
+  inputStripe = deduplicate(inputStripe);
+
+  return inputHubspot.filter((hubspotUser) => {
+    const stripeTransactions = inputStripe.filter((transaction) => {
+      const fName = transaction.name.split(' ')[0];
+      const lName = transaction.name.split(' ').pop();
+      return (
+        fName === hubspotUser['First Name'] &&
+        lName === hubspotUser['Last Name']
+      );
+    });
+    return (
+      ['Open Offer', 'Current Customer'].includes(hubspotUser['Lead Status']) &&
+      stripeTransactions.length > 1
+    );
+  });
 }
 
 export function getTotalValueOfAllCustomers(input) {
-  return input;
+  return [
+    {
+      total: input.reduce((total, user) => {
+        return total + parseInt(user['Total Value']);
+      }, 0),
+    },
+  ];
 }
 
 export function getUsersWithNonMatchingEmails(inputMailchimp, inputStripe) {
-  return [];
+  inputStripe = deduplicate(inputStripe);
+
+  return inputMailchimp
+    .map((user) => {
+      const stripeTransaction = inputStripe.find((transaction) => {
+        const fName = transaction.name.split(' ')[0];
+        const lName = transaction.name.split(' ').pop();
+        return fName === user.firstName && lName === user.lastName;
+      });
+
+      return {
+        mailchimpEmail: user.emailAddress,
+        mailchimpStatus: user.status,
+        mailchimpAudienceName: user.audienceName,
+        mailchimpSubscribeDate: user.subscribeDate,
+        stripeId: stripeTransaction?.id,
+        stripeEmail: stripeTransaction?.email,
+        stripeCreatedAt: stripeTransaction?.created_at_date,
+      };
+    })
+    .filter(
+      (user) => user.stripeEmail && user.mailchimpEmail !== user.stripeEmail,
+    );
 }
